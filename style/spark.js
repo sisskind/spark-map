2ef20deef1e0ce19991c2e487770a5465aaf23eb
Josh Sisskind <6191659+sisskind@users.noreply.github.com>
2021-11-22 11:14:18 -0500

    Move styles, function to JS

---
+++ style/spark.js
@@ -0,0 +1,70 @@
+function addPopUp(feature, layer){
+				var popupTxt = "You clicked an amazing feature!";
+				layer.bindPopup(popupTxt);
+		}
+
+// List of styles
+var buildingsStyle = {
+	color: "#E22B2B", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var otherStyle = {
+	color: "#FFFF00", 
+	weight: 5, 
+	opacity: 0.5
+};
+
+var OwaysStyle = {
+	color: "#E22B2B", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var EwaysStyle = {
+	color: "#38f5a3", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var EbuildingsStyle = {
+	color: "#E22B2B", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var schoolStyle = {
+	color: "#15cf5c", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var houseStyle = {
+	color: "#6f15cf", 
+	weight: 5, 
+	opacity: 0.70
+};
+	
+var apartmentsStyle = {
+	color: "#8a427f", 
+	weight: 5, 
+	opacity: 0.70}
+	
+var garageStyle = {
+	color: "#42898a", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var garagesStyle = {
+	color: "#c9400e", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var yesStyle = {
+	color: "#edf505", 
+	weight: 5, 
+	opacity: 0.70
+};
