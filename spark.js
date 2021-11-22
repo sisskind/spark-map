9774e34bc047012c70cdc3f75886c0d8f2638e89
Josh Sisskind <6191659+sisskind@users.noreply.github.com>
2021-11-22 13:44:43 -0500

    updated center point

--- spark.js
+++ spark.js
@@ -1,6 +1,31 @@
 function addPopUp(feature, layer){
-		var popupTxt = "You clicked an amazing feature!";
-		layer.bindPopup(popupTxt);
+			var popupTxt = "";
+			var osmURL = ""
+			// Try to get the name of the feature
+			if (feature.properties && feature.properties.name) {
+				popupTxt += `<b>${feature.properties.name}</b>`;
+			}
+
+		    // does this feature have a property named id?
+		    if (feature.properties && feature.properties['@id']) {
+		    	// Excellent! We can now link directly to the feature
+		    	osmURL = `https://www.openstreetmap.org/${feature.properties['@id']}`
+		    } else if (feature.properties && feature.properties['id']) {
+		    	// Excellent! We can now link directly to the feature
+		    	osmURL = `https://www.openstreetmap.org/${feature.properties['id']}`
+		    } else if (feature.properties && feature.geometry.coordinates) {
+		    	// Since we don't have the ID, let's try for the centroid of the feature
+		    	var centroid;
+		    	if (feature.geometry.type === 'Polygon') {
+		    		centroid = turf.centroid(feature).geometry.coordinates;
+		    	}
+		    	osmURL = `https://www.openstreetmap.org/edit/#map=${map.getZoom()}/${centroid[0]}/${centroid[0]}`;
+		    }
+
+		    if (osmURL != "") {
+		    	popupTxt += `</br></br>View this feature on <a target="_blank" href=${osmURL}>OSM</a>!`
+		    }
+		    layer.bindPopup(popupTxt);
 }
 
 function getXML(query, lyrControl){
