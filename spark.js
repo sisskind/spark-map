3358df94eb5f1ee69dfa0db5b353321b2658039f
Josh Sisskind <6191659+sisskind@users.noreply.github.com>
2021-12-09 01:21:55 -0500

    Update spark.js

--- spark.js
+++ spark.js
@@ -1,18 +1,18 @@
 function addPopUp(feature, layer){
 			var popupTxt = "";
 			var osmURL = ""
-			// Try to get the name of the feature
-			if (feature.properties && feature.properties.name) {
-				popupTxt += `<b>${feature.properties.name}</b>`;
-			}
 
 		    // does this feature have a property named id?
 		    if (feature.properties && feature.properties['@id']) {
 		    	// Excellent! We can now link directly to the feature
-		    	osmURL = `https://www.openstreetmap.org/${feature.properties['@id']}`
+		    	featureID = feature.properties['@id'];
+		    	osmURL = `https://www.openstreetmap.org/${featureID}`
+		    	popupTxt = `<b>OSM ${featureID.split('/')[0]} ID: ${featureID.split('/')[1]}</b>`;
 		    } else if (feature.properties && feature.properties['id']) {
 		    	// Excellent! We can now link directly to the feature
-		    	osmURL = `https://www.openstreetmap.org/${feature.properties['id']}`
+		    	featureID = feature.properties['id'];
+		    	osmURL = `https://www.openstreetmap.org/${featureID}`
+		    	popupTxt = `<b>OSM ${featureID.split('/')[0]} ID: ${featureID.split('/')[1]}</b>`;
 		    } else if (feature.properties && feature.geometry.coordinates) {
 		    	// Since we don't have the ID, let's try for the centroid of the feature
 		    	var centroid;
@@ -21,6 +21,12 @@ function addPopUp(feature, layer){
 		    	}
 		    	osmURL = `https://www.openstreetmap.org/edit/#map=${map.getZoom()}/${centroid[0]}/${centroid[0]}`;
 		    }
+
+		    // Try to get the name of the feature
+			if (feature.properties && feature.properties.name) {
+				popupTxt = `<b>${feature.properties.name}</b>`;
+			}
+
 
 		    if (osmURL != "") {
 		    	popupTxt += `</br></br>View this feature on <a target="_blank" href=${osmURL}>OSM</a>!`
