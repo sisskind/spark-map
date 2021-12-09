f08709f609a9c56100d8a4cc6d4e4f16c6cbd1ac
Josh Sisskind <6191659+sisskind@users.noreply.github.com>
2021-12-09 13:53:13 -0500

    add styles back

--- spark.js
+++ spark.js
@@ -77,3 +77,82 @@ function getXML(query, lyrControl){
     	}
 	});
 }
+
+var buildingsStyle = {
+	color: "#E22B2B", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var otherStyle = {
+	color: "#FFFF00", 
+	weight: 5, 
+	opacity: 0.5
+};
+
+var OwaysStyle = {
+	color: "#E22B2B", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var EwaysStyle = {
+	color: "#38f5a3", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var EbuildingsStyle = {
+	color: "#E22B2B", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var schoolStyle = {
+	color: "#15cf5c", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var houseStyle = {
+	color: "#6f15cf", 
+	weight: 5, 
+	opacity: 0.70
+};
+	
+var apartmentsStyle = {
+	color: "#8a427f", 
+	weight: 5, 
+	opacity: 0.70}
+	
+var garageStyle = {
+	color: "#42898a", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var garagesStyle = {
+	color: "#c9400e", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var yesStyle = {
+	color: "#edf505", 
+	weight: 5, 
+	opacity: 0.70
+};
+
+var residentialStyle = {
+	color: "#24ff5e",
+	weight: 5,
+	opactiy: 0.70
+};	
+
+var serviceStyle = {
+	color: "#ff7621",
+	weight: 5,
+	opacity: 0.70
+};
+
+var styleList = {};
